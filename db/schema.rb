c3f5f547f7a9557aa7efaf5b609195dc89e05fd8
PMM217 <x14395411@student.ncirl.ie>
2024-08-12 20:24:46 +0100

    command update

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_11_160646) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_11_160802) do
   create_table "courses", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "enrollments", force: :cascade do |t|
+    t.integer "student_id", null: false
+    t.integer "course_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["course_id"], name: "index_enrollments_on_course_id"
+    t.index ["student_id"], name: "index_enrollments_on_student_id"
   end
 
   create_table "students", force: :cascade do |t|
@@ -25,4 +34,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_11_160646) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "enrollments", "courses"
+  add_foreign_key "enrollments", "students"
 end
